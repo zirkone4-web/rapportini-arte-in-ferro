5862f070585f87a3e75a4707363e7cdb5209171b
zirkone4-web <zirkone4@gmail.com>
2026-07-27 23:59:08 +0200

    Completa la nuova schermata accessi dipendenti

--- apps/windows/ArteInFerro.Rapportini.Desktop/Views/EmployeeAccessWindow.xaml.cs
+++ apps/windows/ArteInFerro.Rapportini.Desktop/Views/EmployeeAccessWindow.xaml.cs
@@ -1,13 +1,26 @@
 using System.Windows;
+using ArteInFerro.Rapportini.Desktop.Services;
 using ArteInFerro.Rapportini.Desktop.ViewModels;
 
 namespace ArteInFerro.Rapportini.Desktop.Views;
 
 public partial class EmployeeAccessWindow : Window
 {
-    public EmployeeAccessWindow(EmployeeAccessViewModel viewModel)
+    private readonly SupabaseApiService _api;
+    private readonly EmployeeAccessViewModel _viewModel;
+
+    public EmployeeAccessWindow(EmployeeAccessViewModel viewModel, SupabaseApiService api)
     {
         InitializeComponent();
+        _api = api;
+        _viewModel = viewModel;
         DataContext = viewModel;
+    }
+
+    private async void CreateUserClick(object sender, RoutedEventArgs e)
+    {
+        var window = new EmployeeCreateWindow(new EmployeeCreateViewModel(_api)) { Owner = this };
+        if (window.ShowDialog() == true)
+            await _viewModel.LoadAsync();
     }
 }
